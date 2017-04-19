faf02b46fa129cd67dfa720760ca98373900fa48
ALUMNEDAM <ALUMNEDAM@192.168.120.63>
2017-04-19 19:28:21 +0200

    Alehop

---
+++ M9UF1A1/src/m9uf1a1/SeguretatAES.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m9uf1a1;
+
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+
+/**
+ *
+ * @author ALUMNEDAM
+ */
+public class SeguretatAES {
+    
+    void crearContrasenya() {
+        int keySize = 0;
+        String key;
+        KeyPairGenerator generator = null;
+        while (keySize != 128 && keySize != 192 && keySize != 256) {
+            System.out.println("Introdueix el tamany de la clau (128, 192 o 256): ");
+            Scanner sc = new Scanner(System.in);
+            keySize = sc.nextInt();
+            KeyPair claveGenerada = generator.genKeyPair();
+            
+        }
+    }
+    
+   void xifrarFitxer(String fitxer, SecretKey clau) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
+       
+        Cipher cipher = Cipher.getInstance(fitxer);
+        cipher.init(Cipher.ENCRYPT_MODE, clau);
+        
+    }
+}
