84a834e5038f67fb594b32528b27096406265e97
ALUMNEDAM <ALUMNEDAM@192.168.120.63>
2017-05-16 21:07:23 +0200

    final

--- M9UF1A6/src/m9uf1a6/Desti.java
+++ M9UF1A6/src/m9uf1a6/Desti.java
@@ -5,11 +5,62 @@
  */
 package m9uf1a6;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PrivateKey;
+import java.security.SignatureException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.Enumeration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import static m9uf1a6.M9UF1A6.ficheroOrigen;
+
 /**
  *
  * @author ALUMNEDAM
  */
 public class Desti {
-    
+
+    /*
+    * Metodo para descifrar y comprovar el firmado.
+    */
+    public void descifrar() {
+        try {
+            //Instanciamos origen
+            Origen o = new Origen();
+            //Llamamos al metodo y asi generamos el keystore
+            o.ametodo(ficheroOrigen, "123456");
+            KeyStore ko = o.getKs();
+            //Creamos una enumeracion con los datos del keystore
+            Enumeration<String> aliases = ko.aliases();
+            
+            while (aliases.hasMoreElements()) { //Mientras tenga elementos imprime el siguiente
+                System.out.println(aliases.nextElement());
+            }
+            //Instanciamos una privateKey
+            PrivateKey pkey = (PrivateKey) ko.getKey("origen", "123456".toCharArray());
+            //Instanciamos el objeto certificado para poder certificar la clave 
+            X509Certificate c = (X509Certificate) ko.getCertificate("desticert");
+            //Imprime la clave publica y verifica esta
+            System.out.println(c.getPublicKey());
+            c.verify(c.getPublicKey());
+
+        } catch (NoSuchAlgorithmException | UnrecoverableKeyException | CertificateException | InvalidKeyException | NoSuchProviderException | SignatureException ex) {
+            Logger.getLogger(M9UF1A6.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(M9UF1A6.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(M9UF1A6.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (KeyStoreException ex) {
+            Logger.getLogger(Desti.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
 }
--- M9UF1A6/src/m9uf1a6/Origen.java
+++ M9UF1A6/src/m9uf1a6/Origen.java
@@ -5,13 +5,18 @@
  */
 package m9uf1a6;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.security.InvalidKeyException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.Signature;
+import java.security.SignatureException;
 import java.security.cert.CertificateException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -23,29 +28,72 @@ import static m9uf1a6.M9UF1A6.ficheroOrigen;
  */
 public class Origen {
 
+    //Guardamos en una variable Keystore el almacen que pasaremos al destino.
     KeyStore ks;
     String texto;
 
+    //Construtor
     public Origen(String texto) {
         this.texto = texto;
     }
 
-    Origen() {
+    public Origen() {
     }
 
+    /*
+    * Metodo que cifra un fichero con la contraseña pasada por parametro.
+     */
     public void ametodo(String ksfile, String passwd) throws IOException, NoSuchAlgorithmException, CertificateException {
 
         try {
+            //Instanciamos el keystore en JCEKS.
             ks = KeyStore.getInstance("JCEKS");
+            //Creamos un inputStream para escribir en el fichero
             FileInputStream fis1 = new FileInputStream(ficheroOrigen);
-            ks.load(fis1, "123456".toCharArray());
+            //Instanciamos un file
             File f = new File(ksfile);
-            if (f.isFile()) {
-                FileInputStream fis2 = new FileInputStream(f);
+            if (f.isFile()) {   //Si es de tipo file
+                //Carga el keyStore desde el inputStream
+                ks.load(fis1, "123456".toCharArray());
             }
         } catch (KeyStoreException | FileNotFoundException ex) {
             Logger.getLogger(Origen.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+
+    /*
+    * Metodo que firma un fichero.
+     */
+    public byte[] signData(String fitxer, PrivateKey priv) {
+        //Variables a instanciar
+        byte[] signature = null;
+        FileInputStream fis;
+        BufferedInputStream bis;
+        byte[] buffer = new byte[1024];
+        int mida;
+
+        try {
+            //Instanciamos el bufferedInput i el fileInput para leer el fichero.
+            fis = new FileInputStream(fitxer);
+            bis = new BufferedInputStream(fis);
+
+            //Inicializamos el signer para la firma.
+            Signature signer = Signature.getInstance("SHA1withRSA");
+            signer.initSign(priv); //Inicialitzem la firma digital a partir de l ’algorisme utilitzat
+
+            //Recorremos el archivo.
+            while (bis.available() != 0) {
+                mida = bis.read(buffer);
+                signer.update(buffer, 0, mida); //Le asignamos la firma al objeto signer.
+            }
+
+            //Se cierra el recurso.
+            bis.close();
+            signature = signer.sign(); //Se firma.
+        } catch (NoSuchAlgorithmException | InvalidKeyException | IOException | SignatureException ex) {
+            System.err.println("Error signant les dades: " + ex);
+        }
+        return signature;
     }
 
     public KeyStore getKs() {
